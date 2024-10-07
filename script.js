8fa67bedf79e8bb193f5c4e5235ad7b3235d0339
codingwithfelony <qasimshahzad1261@gmail.com>
2024-10-07 14:54:28 -0700

    Main Files

---
+++ script.js
@@ -0,0 +1,25 @@
+var main = document.querySelector("#main");
+var cursor = document.querySelector("#cursor");
+var image = document.querySelector("#image");
+main.addEventListener("mousemove", function (dets) {
+  gsap.to(cursor, {
+    x: dets.x,
+    y: dets.y,
+    duration: 1,
+    ease: "back.out",
+  });
+});
+image.addEventListener("mouseenter", function () {
+  cursor.innerHTML = "View More";
+  gsap.to(cursor, {
+    scale: 4,
+    backgroundColor: "#ffffff8a",
+  });
+});
+image.addEventListener("mouseleave", function () {
+  cursor.innerHTML = "";
+  gsap.to(cursor, {
+    scale: 1,
+    backgroundColor: "#fff",
+  });
+});
